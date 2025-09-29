d45d400ea464d1f58289e90ee338abb00218fe1d
AliJohnnaton <trofimov2002@mail.ru>
2025-09-29 18:03:07 +0300

    Scripts

--- scripts421.sql
+++ scripts421.sql
@@ -1,30 +1,8 @@
-UPDATE student
-SET age = 20
-WHERE age IS NULL;
-
-UPDATE student
-SET age = 16
-WHERE age < 16;
-
-DELETE FROM student s
-WHERE s.id NOT IN (
-    SELECT MIN(s2.id)
-    FROM student s2
-    GROUP BY s2.name
-);
-
 ALTER TABLE student
-ADD CONSTRAINT uq_student_name UNIQUE (name);
-
-ALTER TABLE student
-ADD CONSTRAINT chk_student_age CHECK (age >= 16);
-
-DELETE FROM faculty f
-WHERE f.id NOT IN (
-    SELECT MIN(f2.id)
-    FROM faculty f2
-    GROUP BY f2.name, f2.color
-);
+    ADD CONSTRAINT chk_student_age CHECK (age >= 16),
+    ADD CONSTRAINT uq_student_name UNIQUE (name),
+    ALTER COLUMN name SET NOT NULL,
+    ALTER COLUMN age SET DEFAULT 20;
 
 ALTER TABLE faculty
-ADD CONSTRAINT uq_faculty_name_color UNIQUE (name, color);
+    ADD CONSTRAINT uq_faculty_name_color UNIQUE (name, color);
